6c5a69e218834ebf49fae725d64c9b8263ca59ee
Claire <clairelogan16@gmail.com>
2021-07-29 16:07:34 -0700

    foundation

---
+++ my-website/src/Components/App.js
@@ -0,0 +1,16 @@
+import '../App.css'
+import Nav from './Nav'
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <div className="Container">
+
+      </div>
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ my-website/src/Components/Nav.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function Nav() {
+    return (
+        <div class="ui inverted segment">
+            <div class="ui inverted secondary pointing menu">
+                <a class="item active">
+                Home
+                </a>
+                <a class="item">
+                About
+                </a>
+                <a class="item">
+                Projects
+                </a>
+                <a class="item">
+                Resume
+                </a>
+            </div>
+        </div>
+    )
+}
+
+export default Nav
